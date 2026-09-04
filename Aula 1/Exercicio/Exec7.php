3512f2f48b17d2c14675a0cf8c805b65189b0dea
Davi <davibote20066@gmail.com>
2026-09-03 21:47:18 -0300

    exec7Final

--- Aula 1/Exercicio/Exec7.php
+++ Aula 1/Exercicio/Exec7.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     <h1> CALCULADOR DE ÁREA DE RETÂNGULO </h1>
-    <form method="POST" action = "Resposta/Resp7.php">
+    <form method="POST" action = "../Resposta/Resp7.php">
     <label for="valor1"> INFORME O VALOR DA BASE </label>
     <input type= "number" id = "valor1" name="valor1">
     <label for="valor2"> INFORME O VALOR DA ALTURA </label>
-    <input type="number" id ="valor2" namer="valor2">
+    <input type="number" id ="valor2" name="valor2">
     <button type="submit">CALCULAR ÁREA </button>
     
 </body>

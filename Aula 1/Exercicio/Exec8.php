915efa804406ed8894b1ca78e75ff8138d69b930
Davi Bote <davibote20066@gmail.com>
2026-09-07 13:43:43 -0300

    Exercicio8

---
+++ Aula 1/Exercicio/Exec8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 8</title>
+</head>
+<body>
+    <h1> Área da Cincunferência </h1>
+    <form method = "POST" action = "../respostas/Resp8.php">
+        <label for="valor1"> Informe o raio da circunferência: </label>
+        <input type="number" id="valor1" name="valor1">
+        <button type="submit"> CALCULAR ÁREA DA CIRCUNFERÊNCIA </button>
+    </form>
+    
+</body>
+</html>
---
+++ Aula 1/Resposta/Resp8.php
@@ -0,0 +1,4 @@
+<?php
+    $valor1 = $_POST['valor1'];
+    $area = ($valor1 * $valor1) * 3.14;
+    echo "A área da circunferência é: " .$area;

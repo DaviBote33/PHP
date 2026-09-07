fd252cba86a9c3ba4e1c026c4de90fb3fab0960a
Davi Bote <davibote20066@gmail.com>
2026-09-07 18:38:53 -0300

    Exec10

---
+++ Aula 1/Exercicio/Exec10.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Exercício 10 </title>
+</head>
+<body>
+    <h1> CALCULAR PERÍMETRO DA CIRCUNFERÊNCIA </h1>
+    <form method="POST" action="../respostas/Resp10.php">
+        <label for="valor1"> INFORME O RAIO DA CIRCUNFERÊNCIA: </label>
+        <input type="number" id="valor1" name="valor1">
+        <button type="submit"> CALCULAR PERÍMETRO </button>
+    </form>
+    
+</body>
+</html>
---
+++ Aula 1/Resposta/Resp10.php
@@ -0,0 +1,4 @@
+<?php   
+    $valor1 = $_POST['valor1'];
+    $perimetro = 2 * 3.14 * $valor1;
+    echo "O perímetro da circunferência é: ".$perimetro;

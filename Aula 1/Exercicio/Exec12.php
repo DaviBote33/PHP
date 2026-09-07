c2337ac608e9769c99330dca50375e7a81d2a276
Davi Bote <davibote20066@gmail.com>
2026-09-07 20:34:50 -0300

    Exec12

---
+++ Aula 1/Exercicio/Exec12.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>METROS PARA CENTÍMETROS</title>
+</head>
+<body>
+    <h1> METROS PARA CENTÍMETROS </h1>
+    <form method="POST" action="../respostas/Resp12.php">
+        <label for="valor1"> Informe o valor em metros: </label>
+        <input type="number" id="valor1" name="valor1">
+        <button type="submit"> CONVERTER PARA CENTÍMETROS </button>
+    </form>
+</body>
+</html>
---
+++ Aula 1/Resposta/Resp12.php
@@ -0,0 +1,4 @@
+<?php
+    $valor1 = $_POST['valor1'];
+    $metros = $valor1 * 100;
+    echo "O valor em centímetros é: ".$metros;

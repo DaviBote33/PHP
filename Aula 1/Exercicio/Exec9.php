341f9a865ad44d355bed077d755230b15686e2d2
Davi Bote <davibote20066@gmail.com>
2026-09-07 18:03:03 -0300

    exec9

---
+++ Aula 1/Exercicio/Exec9.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <h1> PERÍMETRO DO RETÂNGULO </h1>
+    <form method="POST" action="../respostas/Resp9.php">
+        <label for="valor1"> Informe o valor da base do retânguolo: </label>
+        <input type="number" id="valor1" name="valor1">
+        <label for="valor2"> Informe o valor da altura do retânguolo: </label>
+        <input type="number" id="valor2" name="valor2">
+        <input type="submit" value="Calcular Perímetro">
+    </form>
+</body>
+</html>
---
+++ Aula 1/Resposta/Resp9.php
@@ -0,0 +1,5 @@
+<?php
+    $valor1 = $POST['valor1'];
+    $valor2 = $POST['valor2'];
+    $perimetro = 2 * ($valor1 + $valor2);
+    echo "O perímetro do retângulo é: ".$perimetro;

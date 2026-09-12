0a46f8b4a634fdb18e5437178622a75fa5c667da
Davi Bote <davibote20066@gmail.com>
2026-09-11 22:39:39 -0300

    Exec7Feito

---
+++ Aula 2/Exercicio/Exec7.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>CONTAGEM REGRESSIVA</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body> 
+
+<div class="container py-3">
+
+<h1>CONTAGEM REGRESSIVA</h1>
+
+<form method="post">
+
+    <div class="mb-3">
+        <label for="numero1" class="form-label">
+            Insira o inicio da contagem:
+        </label>
+
+        <input
+            type="number"
+            id="numero1"
+            name="numero1"
+            class="form-control"
+            required=""
+        >
+    </div>
+
+    <button type="submit" class="btn btn-primary">
+        Enviar
+    </button>
+
+    <?php
+
+    if($_POST){
+
+        $numero1 = $_POST['numero1'];
+
+        do {
+
+            echo $numero1 . "<br>";
+
+            $numero1--;
+
+        } while($numero1 >= 1);
+    }
+
+    ?>
+
+</form>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+    crossorigin="anonymous">
+</script>
+
+</div>
+
+</body>
+</html>

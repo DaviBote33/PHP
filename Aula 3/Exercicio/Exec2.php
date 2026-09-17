0c4bcf8c4a321e6dfa6975c82e11ff69dbaee90f
Davi Bote <davibote20066@gmail.com>
2026-09-17 16:04:21 -0300

    exec123

---
+++ Aula 3/Exercicio/Exec2.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Maiúsculo e Minúsculo</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container py-3">
+
+<h1>Maiúsculo e Minúsculo</h1>
+
+<form method="post">
+
+    <div class="mb-3">
+        <label for="palavra" class="form-label">Digite uma palavra:</label>
+        <input type="text" id="palavra" name="palavra" class="form-control" required="">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+
+<?php
+    if($_POST){
+        $palavra = $_POST['palavra'];
+
+        echo "<p>Maiúsculo: " . strtoupper($palavra) . "</p>";
+        echo "<p>Minúsculo: " . strtolower($palavra) . "</p>";
+    }
+?>
+
+</form>
+
+</div>
+</body>
+</html>
